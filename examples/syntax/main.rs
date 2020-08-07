efea43cd5eeb16ea3642077dbfd8db5ebb069846
tahscenery <tahscenery@gmail.com>
2020-08-08 02:23:01 +1000

    Moved example into separate folder

--- examples/syntax/main.rs
+++ examples/syntax/main.rs
@@ -1,14 +1,22 @@
-mod syntax;
+mod node;
+mod token;
 
 use cavea::Arena;
-use syntax::node::*;
-use syntax::token::*;
+use node::{
+    SyntaxNode as SN,
+    SyntaxNodeKind as SNK,
+};
+use token::{
+    RawSyntaxTokenData as RSData,
+    SyntaxToken as ST,
+    SyntaxTokenKind as STK,
+};
 use std::rc::Rc;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum Syntax {
-    Node(SyntaxNode),
-    Token(SyntaxToken),
+    Node(SN),
+    Token(ST),
 }
 
 fn main() {
@@ -25,40 +33,44 @@ fn main() {
     //       2
     let arena = &mut Arena::new();
 
+    use Syntax as S;
+    use token::*;
+    use node::*;
+
     let raw_token_symbol_minus =
-        Rc::new(RawSyntaxTokenData::new(SyntaxTokenKind::Symbol(Symbol::Minus), "-"));
+        Rc::new(RSData::new(STK::Symbol(Symbol::Minus), "-"));
     let raw_token_symbol_asterisk =
-        Rc::new(RawSyntaxTokenData::new(SyntaxTokenKind::Symbol(Symbol::Asterisk), "*"));
+        Rc::new(RSData::new(STK::Symbol(Symbol::Asterisk), "*"));
     let raw_token_identifier_a =
-        Rc::new(RawSyntaxTokenData::new(SyntaxTokenKind::Identifier, "a"));
+        Rc::new(RSData::new(STK::Identifier, "a"));
     let raw_token_symbol_plus =
-        Rc::new(RawSyntaxTokenData::new(SyntaxTokenKind::Symbol(Symbol::Plus), "+"));
+        Rc::new(RSData::new(STK::Symbol(Symbol::Plus), "+"));
     let raw_token_literal_two =
-        Rc::new(RawSyntaxTokenData::new(SyntaxTokenKind::Literal(Literal::Integer), "2"));
+        Rc::new(RSData::new(STK::Literal(Literal::Integer), "2"));
     let raw_token_literal_ten =
-        Rc::new(RawSyntaxTokenData::new(SyntaxTokenKind::Literal(Literal::Integer), "2"));
+        Rc::new(RSData::new(STK::Literal(Literal::Integer), "2"));
 
     let node_expr_binary =
-        Rc::new(Syntax::Node(SyntaxNode::new(SyntaxNodeKind::Expr(Expr::Binary))));
+        Rc::new(S::Node(SN::new(SNK::Expr(Expr::Binary))));
     let node_expr_unary =
-        Rc::new(Syntax::Node(SyntaxNode::new(SyntaxNodeKind::Expr(Expr::Unary))));
+        Rc::new(S::Node(SN::new(SNK::Expr(Expr::Unary))));
 
     let token_symbol_minus_1 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_symbol_minus), 13, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_symbol_minus), 13, 1)));
     let token_symbol_asterisk_1 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_symbol_asterisk), 2, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_symbol_asterisk), 2, 1)));
     let token_identifier_a_1 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_identifier_a), 0, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_identifier_a), 0, 1)));
     let token_symbol_plus_1 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_symbol_plus), 8, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_symbol_plus), 8, 1)));
     let token_symbol_minus_2 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_symbol_minus), 5, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_symbol_minus), 5, 1)));
     let token_literal_two_1 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_literal_two), 6, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_literal_two), 6, 1)));
     let token_identifier_a_2 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_identifier_a), 10, 1)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_identifier_a), 10, 1)));
     let token_literal_ten_1 =
-        Rc::new(Syntax::Token(SyntaxToken::new(Rc::clone(&raw_token_literal_ten), 15, 2)));
+        Rc::new(S::Token(ST::new(Rc::clone(&raw_token_literal_ten), 15, 2)));
 
     let arena_token_minus_1 =
         arena.insert(Rc::clone(&token_symbol_minus_1));
